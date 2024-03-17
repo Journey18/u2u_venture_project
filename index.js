05e10ce612e72209cadf41d469772a305e33f00a
Journey18 <nghia.nguyentrung18@gmail.com>
2024-03-17 09:38:58 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,11 @@
+var chuoi = "Helloworld";
+
+function daoNguocChuoi() {
+  var arrChuoi = chuoi.split("");
+  console.log(arrChuoi);
+  var arrDaoNguoc = arrChuoi.reverse();
+  console.log(arrDaoNguoc);
+  return;
+}
+
+daoNguocChuoi();
